4a83c65b194fe363eeaa3936b1c59124187b69f0
Juanes7222 <133039607+Juanes7222@users.noreply.github.com>
2025-10-31 15:35:13 -0500

    Deleted console.log unnecessary

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import StatsPanel from './components/StatsPanel';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const API = `${BACKEND_URL}/api`;
+console.log('Using backend URL:', BACKEND_URL);
 
 function App() {
   const [jobs, setJobs] = useState([]);
